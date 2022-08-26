4f6ff13c7bfc6e57f19493733ea5c3314f2bb535
Alan Truong <alntruong@ucdavis.edu>
2022-08-26 15:31:25 -0700

    moved commented out items into app.post

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 //for when user tries to go to home page
 app.get("/", function(req, res){
@@ -9,29 +12,29 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
- console.log("Post request received.")
+ const query = req.body.cityName;
+ const apiKey = "0ac22548ca215f589c16ceea4ead9bfc";
+ const unit = "imperial"
+ const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+
+ https.get(url, function (response) {
+  console.log(response.statusCode);
+
+  response.on('data', function (data) {
+   const weatherData = JSON.parse(data)
+   const temp = weatherData.main.temp
+   const weatherDescription = weatherData.weather[0].description
+   const icon = weatherData.weather[0].icon
+   const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+   res.write("<p>The weather is currently " + weatherDescription + ".<p>");
+   res.write("<h1>The temperature in " + query + " is " + temp + " F");
+   res.write("<br><img src=" + imageURL + ">");
+   res.send()
+  })
+ })
 })
 
-// const query = "Los Angeles";
-// const apiKey = "0ac22548ca215f589c16ceea4ead9bfc";
-// const unit = "imperial"
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
-// https.get(url, function (response) {
-//  console.log(response.statusCode);
-
-//  response.on('data', function (data) {
-//   const weatherData = JSON.parse(data)
-//   const temp = weatherData.main.temp
-//   const weatherDescription = weatherData.weather[0].description
-//   const icon = weatherData.weather[0].icon
-//   const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//   res.write("<p>The weather is currently " + weatherDescription + ".<p>");
-//   res.write("<h1>The temperature in Los Angeles is " + temp + " F");
-//   res.write("<br><img src=" + imageURL + ">");
-//   res.send()
-//  })
-// })
 
 app.listen(3000, function() { 
  console.log("Server is running on port 3000.");
