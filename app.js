38ed8013b34ed7d0780dbe2dcb8b87c8999463a7
Alan Truong <alntruong@ucdavis.edu>
2022-08-26 13:36:55 -0700

    linked html to get and post

--- app.js
+++ app.js
@@ -5,28 +5,33 @@ const app = express();
 
 //for when user tries to go to home page
 app.get("/", function(req, res){
+ res.sendFile(__dirname + "/index.html");
+});
 
- const query = "Los Angeles";
- const apiKey = "0ac22548ca215f589c16ceea4ead9bfc";
- const unit = "imperial"
- const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
-
- https.get(url, function(response){
-  console.log(response.statusCode);
- 
-  response.on('data', function(data){
-   const weatherData = JSON.parse(data)
-   const temp = weatherData.main.temp
-   const weatherDescription = weatherData.weather[0].description
-   const icon = weatherData.weather[0].icon
-   const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-   res.write("<p>The weather is currently " + weatherDescription + ".<p>");
-   res.write("<h1>The temperature in Los Angeles is " + temp + " F");
-   res.write("<br><img src=" + imageURL + ">");
-   res.send()
-  })
- })
+app.post("/", function(req, res){
+ console.log("Post request received.")
 })
+
+// const query = "Los Angeles";
+// const apiKey = "0ac22548ca215f589c16ceea4ead9bfc";
+// const unit = "imperial"
+// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+
+// https.get(url, function (response) {
+//  console.log(response.statusCode);
+
+//  response.on('data', function (data) {
+//   const weatherData = JSON.parse(data)
+//   const temp = weatherData.main.temp
+//   const weatherDescription = weatherData.weather[0].description
+//   const icon = weatherData.weather[0].icon
+//   const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+//   res.write("<p>The weather is currently " + weatherDescription + ".<p>");
+//   res.write("<h1>The temperature in Los Angeles is " + temp + " F");
+//   res.write("<br><img src=" + imageURL + ">");
+//   res.send()
+//  })
+// })
 
 app.listen(3000, function() { 
  console.log("Server is running on port 3000.");
